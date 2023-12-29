a5dc5bd4e29b961cafc64ddc13c31b60f26e55cc
Jatin <rathorejatin168@gmail.com>
2023-12-29 21:14:32 +0530

    toastify added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import BasicLayout from "../components/BasicLayout";
 import { useAuth } from "../hooks/useAuth";
 import { Button } from "antd";
+import { toast } from "react-toastify";
 
 const Home = () => {
   const { isLoggedIn, logout } = useAuth();
@@ -18,6 +19,7 @@ const Home = () => {
         <Button
           onClick={() => {
             logout();
+            toast.info("Logged out successfully", {});
             navigate("/");
           }}
         >
